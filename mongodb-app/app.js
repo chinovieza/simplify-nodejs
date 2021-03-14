ecedbbe9f8458d5e54355a98ec92127469e3128c
Chino Smittikunanon <chino_smi@truecorp.co.th>
2021-03-14 14:23:43 +0700

    Start mongodb app

---
+++ mongodb-app/app.js
@@ -0,0 +1,7 @@
+// const mongoose = require("mongoose");
+// const config = {
+//     autoIndex: true,
+//     useNewUrlParser: true,
+// };
+const connectionString = process.env.MONGODB_CONNECTION;
+console.log(connectionString);
